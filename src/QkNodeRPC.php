61ee65eb965bbdb690b223796e3ca4faa5a15a37
chenjia404 <chenjiablog@qq.com>
2018-12-12 19:02:19 +0800

    init

---
+++ src/QkNodeRPC.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: woshi
+ * Date: 2018/12/12
+ * Time: 18:28
+ */
+namespace quarkblockchain;
+
+class QkNodeRPC extends \EthereumRPC\EthereumRPC
+{
+
+}
---
+++ src/QkToken.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: woshi
+ * Date: 2018/12/12
+ * Time: 18:30
+ */
+namespace quarkblockchain;
+
+class QkToken extends \ERC20\ERC20
+{
+
+}
